985f6e4f8a6f3f2ef7c2f9bf5a6b58803d971216
lindaxiao <lindaxiao.hust@gmail.com>
2017-02-20 09:25:39 +0800

    complete tree page

--- pages/tree/tree.js
+++ pages/tree/tree.js
@@ -1,50 +1,35 @@
+var util = require("../../utils/util.js")
+
 Page({
   data: {
-    list: [
-      {
-        id: 'form',
-        name: '表单',
-        open: false,
-        pages: ['button', 'list', 'input', 'slider', 'uploader']
-      },
-      {
-        id: 'widget',
-        name: '基础组件',
-        open: false,
-        pages: ['article', 'badge', 'flex', 'footer', 'gallery', 'grid', 'icons', 'loadmore', 'panel', 'preview', 'progress']
-      },
-      {
-        id: 'feedback',
-        name: '操作反馈',
-        open: false,
-        pages: ['actionsheet', 'dialog', 'msg', 'picker', 'toast']
-      },
-      {
-        id: 'nav',
-        name: '导航相关',
-        open: false,
-        pages: ['navbar', 'tabbar']
-      },
-      {
-        id: 'search',
-        name: '搜索相关',
-        open: false,
-        pages: ['searchbar']
-      }
-    ]
+    plantImgs: [
+      "http://7xsmkb.com1.z0.glb.clouddn.com/kakumon2.jpg",
+      "http://7xsmkb.com1.z0.glb.clouddn.com/kakumon3.jpg",
+      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1487455584208&di=6c4bfa226385cf59fbd0a10944dc7d5c&imgtype=0&src=http%3A%2F%2Fimg1.gamersky.com%2Fimage2017%2F02%2F20170218_zl_91_8%2Fgamersky_03small_06_2017218205860A.jpg",
+      "http://7xsmkb.com1.z0.glb.clouddn.com/kumamon1.jpeg"
+    ],
+    likeImg : "../../images/like.png",
+    liked: false,
+    currentDate: util.getCurrentDate()
   },
-  kindToggle: function(e) {
-    var id = e.currentTarget.id
-    var list = this.data.list
-    for(let i = 0; i < list.length; i++) {
-      if(list[i].id === id) {
-        list[i].open = !list[i].open
-      } else {
-        list[i].open = false
-      }
+  like: function() {
+    if(this.data.liked) {
+      this.setData({
+        likeImg: "../../images/like.png"
+      })
+      wx.showToast({
+         title: "您不喜欢这棵树",
+         icon: "success"
+      })
+    } else {
+      this.setData({
+        likeImg: "../../images/like_HL.png"
+      })
+      wx.showToast({
+         title: "您喜欢这棵树",
+         icon: "success"
+      })
     }
-    this.setData({
-      list: list
-    })
+    this.data.liked = !this.data.liked
   }
 })
