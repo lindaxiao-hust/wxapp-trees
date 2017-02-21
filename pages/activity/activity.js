c4d85bcb161237dabfd53dbea263083b4e5096b1
lindaxiao <lindaxiao.hust@gmail.com>
2017-02-21 09:35:06 +0800

    add activity

---
+++ pages/activity/activity.js
@@ -0,0 +1,14 @@
+Page({
+  data: {
+      trees: [
+        {
+          id: 1,
+          hasCollected: true
+        },
+        {
+          id: 2,
+          hasCollected: false
+        }
+      ]
+  }
+})
